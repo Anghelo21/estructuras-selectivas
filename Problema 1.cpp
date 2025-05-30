7743462f51b02bc042276369df7a2993ae58e2e0
AngheloCM <angheloprog@gmail.com>
2025-05-30 00:44:06 -0500

    Última optimización del Problema 1.cpp

--- Problema 1.cpp
+++ Problema 1.cpp
@@ -10,13 +10,21 @@ int main(){
 		cout<<"Solo valores positivos";
     } else{
 	cin>> n2;
+	if (n2<0){
+		cout<<"Solo valores positivos";
+	} else{	
 	cin>> n3;
+    if (n3<0){
+    	cout<<"Solo valores positivos";
+	} else{
 	P= (n1+n2+n3)/3;
 	if (P>=70){
 		cout<<"Usted aprueba";
 	} else{
 		cout<<"Usted desaprueba";
 	}
+}
+}
 }
 	return 0;
 }
