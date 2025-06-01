272eba260f9ba88e2cb8b9149720a1ab8ca182e7
AngheloCM <angheloprog@gmail.com>
2025-06-01 01:31:34 -0500

    Optimizando Problema 2.cpp de estructuras selectivas

--- Problema 2.cpp
+++ Problema 2.cpp
@@ -2,15 +2,17 @@
 using namespace std;
 
 int main(){
-	double p, r, f;
-	cout<<"Ingrese el precio del producto: ";
-	cin>> p;
-	if (p>1000){
-		r= p*0.20;
-		f= p-r;
-		cout<<"El precio a pagar es de: S/"<<f<<".";
+	double precio, descuento, PrecioFinal;
+	cout<<"Ingrese el precio del producto: "<<endl;
+	cin>> precio;
+	if (precio<0){
+		cout<<"El precio ingresado no es valido";
+	} else if(precio>1000){
+		descuento= precio*0.20;
+		PrecioFinal= precio-descuento;
+		cout<<"El precio a pagar es de: S/"<<PrecioFinal<<".";
 	} else{
-		cout<<"El precio a pagar es de: S/"<<p<<".";
+		cout<<"El precio a pagar es de: S/"<<precio<<".";
 	}
 	return 0;
 }
